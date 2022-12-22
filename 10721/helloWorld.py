a9f30427b032f837fe9b6e3d826d98ccc1b9e44b
Amey Joshi <ameyj3304@gmail.com>
2022-12-22 22:14:45 +0530

    Done some assignments

--- 10721/helloWorld.py
+++ 10721/helloWorld.py
@@ -1,1 +1,18 @@
-print ("Helo World")
+list1=[1,2,3,4,5,6,7,8,9,10]
+test=False
+for i in list1:
+    for j in list1:
+        if j==1:
+            continue
+        if i==1:
+            print("Number 1 is neither prime nor composite")
+            break
+        if i==j:
+            continue
+        if i%j==0:
+            print(f"Number {i} is not prime number")
+            break
+        else:
+            test=True
+    if test==True:
+        print (f"Number {i} is Prime number")
