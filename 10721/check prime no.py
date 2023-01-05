a135235cb0f4902cc280a7a2dd7eee7194dd7e25
Amey Joshi <ameyj3304@gmail.com>
2023-01-05 17:16:49 +0530

    Cleaning up

--- 10721/check prime no.py
+++ 10721/check prime no.py
@@ -1,11 +1,19 @@
-num=int(input())
-test=True
-for i in range(num):
-    if i==0 or i==1:
-        continue
-    if num%i==0:
-        test=False
-        print("Vibhav")
-        break
-if test:
-    print("Omkar")
+list1=[1,2,3,4,5,6,7,8,9,10]
+for i in list1:
+    test=False
+    for j in list1:
+        if j==1:
+            continue
+        if i==1:
+            print("Number 1 is neither prime nor composite")
+            break
+        if i==j:
+            continue
+        if i%j==0:
+            print(f"Number {i} is not prime number")
+            break
+        else:
+            test=True
+    if test==True:
+        print (f"Number {i} is Prime number")
+        
