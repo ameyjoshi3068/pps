ae46249c5fbbbac0dc28466021925cf5cf249692
Amey Joshi <ameyj3304@gmail.com>
2023-01-20 23:26:23 +0530

    / ‘10721/Experiment.py’

--- 10721/Experiment.py
+++ 10721/Experiment.py
@@ -2,7 +2,8 @@ def prime_factors(num):
     list=[]
     for i in range(2,num):
         quotient=num%i
-        if quotient==0:
+        while quotient==0:
+            quotient=num%i
             list.append(i)
-            
+
 
